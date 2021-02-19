99005bc969e08999adb755aeac2f318d3be9ca54
Kyle Pfromer <kpfromer2@gmail.com>
2021-02-19 09:13:33 -0700

    refactor: remove unused code

--- src/path.rs
+++ src/path.rs
@@ -1,36 +1,7 @@
 use path_clean::PathClean;
 use std::env;
 use std::io;
-use std::path::{Component, Path, PathBuf};
-
-/// Like `canonicalize` for non existing paths.
-/// Taken from: https://github.com/rust-lang/cargo/blob/fede83ccf973457de319ba6fa0e36ead454d2e20/src/cargo/util/paths.rs#L61
-pub fn normalize_path(path: &Path) -> PathBuf {
-    let mut components = path.components().peekable();
-    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
-        components.next();
-        PathBuf::from(c.as_os_str())
-    } else {
-        PathBuf::new()
-    };
-
-    for component in components {
-        match component {
-            Component::Prefix(..) => unreachable!(),
-            Component::RootDir => {
-                ret.push(component.as_os_str());
-            }
-            Component::CurDir => {}
-            Component::ParentDir => {
-                ret.pop();
-            }
-            Component::Normal(c) => {
-                ret.push(c);
-            }
-        }
-    }
-    ret
-}
+use std::path::{Path, PathBuf};
 
 pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
     let path = path.as_ref();
