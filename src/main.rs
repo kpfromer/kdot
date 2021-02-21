00ccdff65ba02fd91ed8ccd8a821ec1753a7de5c
Kyle Pfromer <kpfromer2@gmail.com>
2021-02-20 17:53:53 -0700

    refactor: move module code to file

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,17 @@
 extern crate log;
 extern crate simplelog;
 
-use anyhow::{bail, Context, Result};
-use config::{ModuleConfig, PackageConfig};
+use anyhow::{bail, Result};
 use simplelog::*;
-use std::{collections::HashMap, iter::FromIterator};
-use std::{
-    collections::HashSet,
-    path::{Path, PathBuf},
-};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 mod config;
+mod module;
 mod path;
 mod symlink;
 
-use path::*;
+use module::*;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -37,65 +33,6 @@ enum Command {
     Unlink { modules: Vec<String> },
     /// Unlinks and then relinks the module to the system.
     Sync { modules: Vec<String> },
-}
-
-fn link_module(module: &ModuleConfig) -> Result<()> {
-    let to = absolute_path(&Path::new(&module.location.to))?;
-    let from = absolute_path(&Path::new(&module.location.from))?;
-
-    info!(
-        "Linking \"{}\" to \"{}\"",
-        to.as_os_str().to_str().unwrap(),
-        from.as_os_str().to_str().unwrap(),
-    );
-
-    symlink::link_folder(&to, &from, true)
-        .with_context(|| format!("Failed to link \"{}\" module.", module.name))?;
-
-    info!("Linked \"{}\" module.", module.name);
-
-    Ok(())
-}
-
-fn unlink_module(module: &ModuleConfig) -> Result<()> {
-    info!("Unlinking {} module.", &module.name);
-    symlink::unlink_folder(
-        &PathBuf::from(&module.location.to),
-        &PathBuf::from(&module.location.from),
-        true,
-    )?;
-
-    Ok(())
-}
-
-// TODO: is the best way of handling this?
-// should it be HashSet<String>?
-fn get_matching_modules<'a>(
-    kdot_config: &'a PackageConfig,
-    modules_names: &'a [String],
-) -> HashSet<&'a String> {
-    let config_module_names: HashSet<&'a String> = kdot_config
-        .modules
-        .iter()
-        .map(|module| &module.name)
-        .collect();
-
-    let modules_names: HashSet<&'a String> = HashSet::from_iter(modules_names);
-
-    modules_names
-        .intersection(&config_module_names)
-        .cloned()
-        .collect()
-}
-
-fn get_module_map<'a>(kdot_config: &'a PackageConfig) -> HashMap<String, &'a ModuleConfig> {
-    let mut map: HashMap<String, &'a ModuleConfig> = HashMap::new();
-
-    kdot_config.modules.iter().for_each(|module| {
-        map.insert(module.name.clone(), module);
-    });
-
-    map
 }
 
 fn main() -> Result<()> {
---
+++ src/module.rs
@@ -0,0 +1,70 @@
+use crate::{
+    config::{ModuleConfig, PackageConfig},
+    path::absolute_path,
+    symlink,
+};
+use anyhow::{Context, Result};
+use std::{collections::HashMap, iter::FromIterator};
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+};
+
+pub fn link_module(module: &ModuleConfig) -> Result<()> {
+    let to = absolute_path(&Path::new(&module.location.to))?;
+    let from = absolute_path(&Path::new(&module.location.from))?;
+
+    info!(
+        "Linking \"{}\" to \"{}\"",
+        to.as_os_str().to_str().unwrap(),
+        from.as_os_str().to_str().unwrap(),
+    );
+
+    symlink::link_folder(&to, &from, true)
+        .with_context(|| format!("Failed to link \"{}\" module.", module.name))?;
+
+    info!("Linked \"{}\" module.", module.name);
+
+    Ok(())
+}
+
+pub fn unlink_module(module: &ModuleConfig) -> Result<()> {
+    info!("Unlinking {} module.", &module.name);
+    symlink::unlink_folder(
+        &PathBuf::from(&module.location.to),
+        &PathBuf::from(&module.location.from),
+        true,
+    )?;
+
+    Ok(())
+}
+
+// TODO: is the best way of handling this?
+// should it be HashSet<String>?
+pub fn get_matching_modules<'a>(
+    kdot_config: &'a PackageConfig,
+    modules_names: &'a [String],
+) -> HashSet<&'a String> {
+    let config_module_names: HashSet<&'a String> = kdot_config
+        .modules
+        .iter()
+        .map(|module| &module.name)
+        .collect();
+
+    let modules_names: HashSet<&'a String> = HashSet::from_iter(modules_names);
+
+    modules_names
+        .intersection(&config_module_names)
+        .cloned()
+        .collect()
+}
+
+pub fn get_module_map<'a>(kdot_config: &'a PackageConfig) -> HashMap<String, &'a ModuleConfig> {
+    let mut map: HashMap<String, &'a ModuleConfig> = HashMap::new();
+
+    kdot_config.modules.iter().for_each(|module| {
+        map.insert(module.name.clone(), module);
+    });
+
+    map
+}
